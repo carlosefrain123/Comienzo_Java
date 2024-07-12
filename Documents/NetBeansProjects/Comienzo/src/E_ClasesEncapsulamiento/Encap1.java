cda1ca6e6a19667952eb3b2ea21623032b835ca0
Efrain <carlosefrainchallo1@gmail.com>
2024-07-11 22:43:58 -0500

    10:43 - 11/07/2024 - Encapsulamiento

---
+++ Documents/NetBeansProjects/Comienzo/src/E_ClasesEncapsulamiento/Encap1.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package E_ClasesEncapsulamiento;
+
+/**
+ *
+ * @author USER
+ */
+public class Encap1 {
+    private String nombre;
+    private int edad;
+    private boolean eliminado;
+
+    public Encap1(String nombre, int edad, boolean eliminado) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.eliminado = eliminado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isEliminado() {
+        return eliminado;
+    }
+
+    public void setEliminado(boolean eliminado) {
+        this.eliminado = eliminado;
+    }
+    
+    
+}
---
+++ Documents/NetBeansProjects/Comienzo/src/E_Encapsulamiento/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package E_Encapsulamiento;
+import E_ClasesEncapsulamiento.Encap1;
+/**
+ *
+ * @author USER
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Encap1 persona=new Encap1("Efrain", 20, true);
+        System.out.println(persona.getNombre());
+        
+        //Acá se puede modificar
+        persona.setNombre("José");
+        System.out.println(persona.getNombre());
+    }
+}
