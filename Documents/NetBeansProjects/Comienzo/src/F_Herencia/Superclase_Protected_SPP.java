a309c96a42a01e4bdc00d723a62424b9dba0a877
Efrain <carlosefrainchallo1@gmail.com>
2024-07-12 10:50:46 -0500

    10:50 - 12/07/2024 - Superclase y protected

---
+++ Documents/NetBeansProjects/Comienzo/src/F_Herencia/Superclase_Protected_SPP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package F_Herencia;
+import F_HerenciaClases.SPPClases;
+/**
+ *
+ * @author USER
+ */
+public class Superclase_Protected_SPP {
+    public static void main(String[] args) {
+        SPPClases ej1=new SPPClases("Efrain", 'M', 20, "Simón Bolivar");
+        System.out.println(ej1.toString());
+    }
+}
