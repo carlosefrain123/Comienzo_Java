fa16f5c4bd03768bac9885a99e1b218a33a1040b
Efrain <carlosefrainchallo1@gmail.com>
2024-07-24 11:40:20 -0500

    24/07/2024 - 11:40 - Java Beans

---
+++ Documents/NetBeansProjects/Peliculas/src/modelo/Pelicula.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+JAVABEANS
+ */
+public class Pelicula implements Serializable{
+    private String nombre;
+
+    public Pelicula() {
+    }
+
+    public Pelicula(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return this.nombre;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.nombre);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Pelicula other = (Pelicula) obj;
+        return Objects.equals(this.nombre, other.nombre);
+    }
+    
+}
